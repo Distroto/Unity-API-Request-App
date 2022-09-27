a9dbe8e04e67a1b3573b9f224553dd5c5eea98f1
Abhishek Sokhal <abhisheksokhal2035@gmail.com>
2022-09-27 22:52:16 +0530

    main files

---
+++ Assets/Scripts/Request.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class Request : MonoBehaviour
+{
+    private string url = "https://gorest.co.in/public/v2/users";
+
+    public void GetData()
+    {
+        StartCoroutine(DataRequest());
+    }
+
+    public Text id;
+    public Text name;
+    public Text email;
+    public Text gender;
+    public Text status;
+
+    IEnumerator DataRequest() 
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if(request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+
+            
+            ResponseData responseData = JsonConvert.DeserializeObject<ResponseData>("{\"users\":" + request.downloadHandler.text + "}");
+
+            //Using Random function to get random user data
+
+            int i = Random.Range(0, responseData.users.Length);
+
+            //Displaying the data and assigning it to the respective text fields
+
+            id.text = responseData.users[i].id.ToString();
+            name.text = responseData.users[i].name.ToString();
+            email.text = responseData.users[i].email.ToString();
+            gender.text = responseData.users[i].gender.ToString();
+            status.text = responseData.users[i].status.ToString();
+
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/ResponseData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+[System.Serializable]
+public class ResponseData
+{
+    public ClientData[] users;
+}
+
+[System.Serializable]
+public class ClientData
+{
+    public int id ;
+    public string name ;
+    public string email ;
+    public string gender ;
+    public string status ;
+
+}
